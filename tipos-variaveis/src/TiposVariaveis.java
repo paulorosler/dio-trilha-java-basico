8472bfa204f8944de6e617346b7c2882c2b77170
Paulo Rosler <paulo_rosler@uol.com.br>
2024-08-20 18:39:50 -0300

    Tipos Primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // Tipos primitivos (byte; short; long; float; double; boolean; char)
+
+        final double PI = 314159264f;
+        System.err.println(PI);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+        System.err.println(numeroCurto2);
+
+
+
     }
 }
