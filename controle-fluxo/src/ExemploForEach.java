cf7f28f9d9969a5463d6cb85eafd5de6387142c8
Paulo Rosler <paulo_rosler@uol.com.br>
2024-08-25 18:48:14 -0300

    commit

--- controle-fluxo/src/ExemploForEach.java
+++ controle-fluxo/src/ExemploForEach.java
@@ -2,9 +2,14 @@ public class ExemploForEach {
     public static void main(String[] args) {
         String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
 
+        char jota = 'J';
         for (String aluno : alunos) 
             {
-                System.out.println("O nome do aluno é " + aluno);  
+             //   if (aluno == "FELIPE") continue;
+             //   if (aluno == "MARCOS")  break;
+                if (aluno.charAt(0) == jota) continue;
+                System.out.println("O nome do aluno é " + aluno); 
+                System.out.println("Inicial: " + aluno.charAt(0)); 
             }
     }
 }
