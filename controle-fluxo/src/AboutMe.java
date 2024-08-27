bcc0fc8d9bf052704b465af4c924f8799b958b98
Paulo Rosler <paulo_rosler@uol.com.br>
2024-08-27 15:01:11 -0300

    commit

---
+++ controle-fluxo/src/AboutMe.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+            //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try {
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
+            
+            System.out.println("Digite seu sobrenome");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua idade");
+            int idade = scanner.nextInt();
+            
+            System.out.println("Digite sua altura");
+            double altura = scanner.nextDouble();
+        
+            //imprimindo os dados obtidos pelo usuario
+            System.out.println("Ola, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + "cm ");
+            scanner.close();
+    }
+    catch (java.util.InputMismatchException e) {
+       System.out.println("Os campos idade e altura tem que ser inteiros e numericos");
+    }
+    }
+}
